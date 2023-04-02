4fb3950ca722159dd3c8f8eb220d87c08a1af644
Shamshadz <chaudhary.s.shamshad07@gmail.com>
2023-04-02 06:28:20 +0530

    update

--- inventory_backend/settings.py
+++ inventory_backend/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
+MEDIA_URL = '/images/'        ## change this to '/static/images/' in hosting
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
